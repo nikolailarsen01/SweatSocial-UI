5379ccb007eef4129116ba0a596ea3730213882e
Mikkel Vestergård <mikkel@mikkelsv.dk>
2023-05-08 10:35:42 +0200

    added submit handler to new post

--- components/post/New.vue
+++ components/post/New.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Response } from "~/interfaces/response"
 import { Dialog, DialogPanel, DialogTitle } from '@headlessui/vue'
 
 //@ts-expect-error
@@ -6,6 +7,8 @@ import TagInput from '@mayank1513/vue-tag-input'
 import '@mayank1513/vue-tag-input/dist/TagInput.css'
 
 import UploadImages from "vue-upload-drop-images"
+
+const config = useRuntimeConfig()
 
 const props = defineProps<{ 
   open: boolean
@@ -17,6 +20,7 @@ const emit = defineEmits<{
 
 const form = ref({
   visibility: '',
+  groupID: '',
   post: '',
   tags: [],
   images: [],
@@ -24,7 +28,28 @@ const form = ref({
 })
 
 const handleSubmit = () => {
-
+  let formData = new FormData()
+  
+  formData.append('content', form.value.post)
+  for(var i = 0; i < form.value.images.length; i++){
+    let file = form.value.images[i]
+    
+    formData.append('images[]', file)
+  }
+  
+  $fetch<Response<any>>(config.public.apiURL + "post", {
+    method: "post",
+    headers: {
+      'Authorization': `Bearer ${'6|gcctbbuj6vmrID2C7n86arPLoRboOFN3GAyKUup6'}`,
+    },
+    body: formData,
+  }).then(res => {
+    
+  }).catch(err => {
+    
+  })
+  
+  //emit('close')
 }
 </script>
 
@@ -38,7 +63,7 @@ const handleSubmit = () => {
           </div>
           <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
             <div class="mt-3 text-center sm:mt-0 sm:text-left">
-              {{ form }}
+              {{ form.images }}
               <div class="mb-2 flex">
                 <img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                 <p class="mb-0 ml-2">
@@ -48,90 +73,94 @@ const handleSubmit = () => {
                     <option value="all">All</option>
                     <option value="group">Group</option>
                     <option value="friends">Friends</option>
-                    <option value="private">No one</option>
+                  </select>
+                  <select v-if="form.visibility == 'group'" @change="e => form.groupID = (e.target as HTMLSelectElement).value" class="ml-2 py-0 px-1 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500">
+                    <option value="1">Grupe 1</option>
+                    <option value="2">Group 2</option>
+                    <option value="3">Group 3</option>
                   </select>
                 </p>
               </div>
               <hr class="my-2" />
               <UploadImages @changed="(i) => form.images = i" />
-              <div @input="e => form.post = (e.target as HTMLElement).innerText" contenteditable class="w-full border-0 outline-none resize-none mt-2" placeholder="I just ripped a fat one right next to my gym chrush. Yiksies"></div>
-              <div class="mt-4 mb-2">
-                <label for="tags">Add some tags</label>
-                <tag-input v-model="form.tags" id="tags" />
+                <div @input="e => form.post = (e.target as HTMLElement).innerText" contenteditable class="w-full border-0 outline-none resize-none mt-2" placeholder="I just ripped a fat one right next to my gym chrush. Yiksies"></div>
+                <div class="mt-4 mb-2">
+                  <label for="tags">Add some tags</label>
+                  <tag-input v-model="form.tags" id="tags" />
+                </div>
+                <div class="flex flex-row-reverse space-x-reverse space-x-4 font-mono text-white text-sm font-bold leading-6 my-2">
+                  <button @click="() => 'event'==form.linkable?form.linkable='':form.linkable='event'" :class="form.linkable == 'event' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
+                    <Icon name="uil:schedule" />
+                  </button>
+                  <button @click="() => 'challenge'==form.linkable?form.linkable='':form.linkable='challenge'" :class="form.linkable == 'challenge' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
+                    <Icon name="ph:fire-bold" />
+                  </button>
+                  <button @click="() => 'group'==form.linkable?form.linkable='':form.linkable='group'" :class="form.linkable == 'group' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
+                    <Icon name="material-symbols:groups" />
+                  </button>
+                  <button @click="() => 'mealplan'==form.linkable?form.linkable='':form.linkable='mealplan'" :class="form.linkable == 'mealplan' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
+                    <Icon name="material-symbols:fastfood-outline" />
+                  </button>
+                  <button @click="() => 'workout'==form.linkable?form.linkable='':form.linkable='workout'" :class="form.linkable == 'workout' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
+                    <Icon name="streamline:travel-hotel-dumbell-sports-weights-dumbbell-sport-fitness" />
+                  </button>
+                </div>
+                
+                <div class="mb-2">
+                  <div v-if="form.linkable == 'event'">
+                    Add a event...
+                  </div>
+                  <div v-if="form.linkable == 'challenge'">
+                    Add a challenge...
+                  </div>
+                  <div v-if="form.linkable == 'group'">
+                    Add a group...
+                  </div>
+                  <div v-if="form.linkable == 'mealplan'">
+                    Add a mealplan...
+                  </div>
+                  <div v-if="form.linkable == 'workout'">
+                    Add a workout...
+                  </div>
+                </div>
+                
+                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0" @click="handleSubmit">Post</button>
               </div>
-              <div class="flex flex-row-reverse space-x-reverse space-x-4 font-mono text-white text-sm font-bold leading-6 my-2">
-                <button @click="() => 'event'==form.linkable?form.linkable='':form.linkable='event'" :class="form.linkable == 'event' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
-                  <Icon name="uil:schedule" />
-                </button>
-                <button @click="() => 'challenge'==form.linkable?form.linkable='':form.linkable='challenge'" :class="form.linkable == 'challenge' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
-                  <Icon name="ph:fire-bold" />
-                </button>
-                <button @click="() => 'group'==form.linkable?form.linkable='':form.linkable='group'" :class="form.linkable == 'group' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
-                  <Icon name="material-symbols:groups" />
-                </button>
-                <button @click="() => 'mealplan'==form.linkable?form.linkable='':form.linkable='mealplan'" :class="form.linkable == 'mealplan' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
-                  <Icon name="material-symbols:fastfood-outline" />
-                </button>
-                <button @click="() => 'workout'==form.linkable?form.linkable='':form.linkable='workout'" :class="form.linkable == 'workout' ? 'bg-blue-600' : 'bg-blue-400'" class="w-12 h-12 rounded-lg flex items-center justify-center hover:bg-blue-700 shadow-lg text-[2rem]">
-                  <Icon name="streamline:travel-hotel-dumbell-sports-weights-dumbbell-sport-fitness" />
-                </button>
-              </div>
-
-              <div class="mb-2">
-                <div v-if="form.linkable == 'event'">
-                  Add a event...
-                </div>
-                <div v-if="form.linkable == 'challenge'">
-                  Add a challenge...
-                </div>
-                <div v-if="form.linkable == 'group'">
-                  Add a group...
-                </div>
-                <div v-if="form.linkable == 'mealplan'">
-                  Add a mealplan...
-                </div>
-                <div v-if="form.linkable == 'workout'">
-                  Add a workout...
-                </div>
-              </div>
-
-              <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0" @click="() => {emit('close'); handleSubmit()}">Post</button>
             </div>
-          </div>
-        </DialogPanel>
+          </DialogPanel>
+        </div>
       </div>
-    </div>
-  </Dialog>
-</template>
-
-<style>
-[placeholder]:empty::before {
-  content: attr(placeholder);
-}
-
-ul[data-v-a216c5f8]::-webkit-scrollbar {
-  height: 3px;
-}
-
-ul[data-v-a216c5f8]::-webkit-scrollbar-track {
-  background: transparent; 
-}
-
-ul[data-v-a216c5f8]::-webkit-scrollbar-thumb {
-  background: #8888887c;
-  border-radius: 1rem;
-}
-
-input[data-v-a216c5f8] {
-  border: 1px solid #e3e3e3;
-}
-
-input[data-v-a216c5f8]:focus-visible {
-  outline: none;
-  border-radius: 0.375rem;
-}
-
-.tag[data-v-a216c5f8]{
-  background: rgb(59 130 246 / var(--tw-bg-opacity));
-}
+    </Dialog>
+  </template>
+  
+  <style>
+  [placeholder]:empty::before {
+    content: attr(placeholder);
+  }
+  
+  ul[data-v-a216c5f8]::-webkit-scrollbar {
+    height: 3px;
+  }
+  
+  ul[data-v-a216c5f8]::-webkit-scrollbar-track {
+    background: transparent; 
+  }
+  
+  ul[data-v-a216c5f8]::-webkit-scrollbar-thumb {
+    background: #8888887c;
+    border-radius: 1rem;
+  }
+  
+  input[data-v-a216c5f8] {
+    border: 1px solid #e3e3e3;
+  }
+  
+  input[data-v-a216c5f8]:focus-visible {
+    outline: none;
+    border-radius: 0.375rem;
+  }
+  
+  .tag[data-v-a216c5f8]{
+    background: rgb(59 130 246 / var(--tw-bg-opacity));
+  }
 </style>
